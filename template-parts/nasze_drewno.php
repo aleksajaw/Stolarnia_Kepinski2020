b8ef9e6dbb4d8504f8af9a0709615d18d2b6f884
aleksajaw <57554133+aleksajaw@users.noreply.github.com>
2020-01-14 21:58:37 +0100

    Update nasze_drewno.php

--- template-parts/nasze_drewno.php
+++ template-parts/nasze_drewno.php
@@ -11,20 +11,21 @@
 	
 	<div class="m-auto w-100">
 	
-	<?php foreach($kindsOfWood as $oneKind => $src){
-	
-		if(!is_string($oneKind)){
-			$oneKind = $src;
-		}
-	
-		echo '<div class="col-lg drewno-oneKind position-relative d-inline-flex align-items-center justify-content-center my-5 mx-4">
-				<picture class="drewno-img overflow-hidden position-absolute m-0 border border-white">
-					<source type="image/webp" srcset="'. $folderImgURL . $src .'.webp"> 
-					<img src="'. $folderImgURL . $src .'.png"/>
-				</picture>
-				<h4 class="drewno-oneKind-title m-0 position-absolute">'. $oneKind .'</h4>
-			</div>';
-		}?>
+	<?php foreach($kindsOfWood as $oneKind => $clearName){
+		
+			if(!is_string($oneKind)){
+				$oneKind = $clearName;
+			}
+			$src = $folderImgURL . $clearName;
+
+			echo '<div class="col-lg drewno-oneKind position-relative d-inline-flex align-items-center justify-content-center my-5 mx-4">
+					<picture class="drewno-img overflow-hidden position-absolute m-0 border border-white">
+						<source type="image/webp" srcset="'. $src .'.webp"> 
+						<img src="'. $src .'.png"/>
+					</picture>
+					<h4 class="drewno-oneKind-title m-0 position-absolute">'. $oneKind .'</h4>
+				</div>';
+			}?>
 
 	</div>
 	
